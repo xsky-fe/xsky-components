e0cd5079f2cc28154458156978a3479bf62dc2f2
huangyan <huangyan@xsky.com>
2016-09-16 11:36:30 +0800

    new Table

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Row from './Row'
 import Col from './Col'
 import Button from './Button'
 import Text from './Text'
+import Table from './Table'
 
 function App(props) {
   return (
@@ -14,6 +15,24 @@ function App(props) {
         <Text.blockqoute>
           <Text>hello, world</Text>
         </Text.blockqoute>
+      </Col>
+      <Col md="12">
+        <Table
+          condensed
+          striped
+          hover
+          data={[
+            { name: 1, age: 1 },
+            { name: 2, age: 2 },
+            { name: 3, age: 3 },
+          ]}
+          fields={{
+            name: '姓名',
+            age: '年龄',
+          }}
+          renderHeader={fields => fields}
+          renderBody={values => values}
+        />
       </Col>
     </Row>
   )
